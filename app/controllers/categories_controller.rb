aed262be316d3c361c7f7afe2d9b9654466f85df
Aditya Agarwal <aditya.agarwal@anchanto.com>
2022-03-20 15:37:22 +0530

    Category all action complete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,10 +17,25 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 15)
     end
 
     def index
         @categories = Category.paginate(page: params[:page], per_page: 10)
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+    
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+          flash[:notice] = "Category name updated successfully"
+          redirect_to @category
+        else
+          render 'edit'
+        end
     end
 
     private
